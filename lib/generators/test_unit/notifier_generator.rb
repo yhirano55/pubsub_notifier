92a423f24418ae725bfd6caf8b55fe87563a2922
Yoshiyuki Hirano <yhirano@me.com>
2017-07-13 23:09:42 +0900

    Cosme

--- lib/generators/test_unit/notifier_generator.rb
+++ lib/generators/test_unit/notifier_generator.rb
@@ -2,7 +2,7 @@ module TestUnit
   class NotifierGenerator < ::Rails::Generators::NamedBase
     source_root File.expand_path("../templates", __FILE__)
 
-    def create_spec_file
+    def create_test_file
       template "notifier_test.tt", File.join("test/notifiers", class_path, "#{file_name}_notifier_test.rb")
     end
   end
--- lib/pubsub_notifier/acts_as_notifier.rb
+++ lib/pubsub_notifier/acts_as_notifier.rb
@@ -5,13 +5,13 @@ module PubsubNotifier
     end
 
     module ClassMethods
+      # Override ActionMailer::Base.method_missing
+      # https://github.com/rails/rails/blob/master/actionmailer/lib/action_mailer/base.rb#L576
       def acts_as_notifier
         class_eval do
           class << self
             private
 
-              # Override ActionMailer::Base#method_missing
-              # https://github.com/rails/rails/blob/master/actionmailer/lib/action_mailer/base.rb#L576
               def method_missing(method_name, *args)
                 if action_methods.include?(method_name.to_s)
                   ::ActionMailer::MessageDelivery.new(self, method_name, *args).tap(&:deliver)
