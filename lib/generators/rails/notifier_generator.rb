8fab00896a4a964a22b0ad16710aa833a4c11fc7
Yoshiyuki Hirano <yhirano@me.com>
2017-07-14 10:16:42 +0900

    Fixed generator for Rails

--- lib/generators/rails/notifier_generator.rb
+++ lib/generators/rails/notifier_generator.rb
@@ -1,7 +1,8 @@
 module Rails
   module Generators
-    class NotifierGenerator < ::Rails::Generators::NamedBase
+    class NotifierGenerator < NamedBase
       source_root File.expand_path("../templates", __FILE__)
+      check_class_collision suffix: "Notifier"
 
       def create_notifier_file
         template "notifier.tt", File.join("app/notifiers", class_path, "#{file_name}_notifier.rb")
