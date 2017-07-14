36e0c20ddf6ac0679ea0ca90b85fb06d2dce8bd0
Yoshiyuki Hirano <yhirano@me.com>
2017-07-14 10:16:57 +0900

    Fixed generator for RSpec

--- lib/generators/rspec/notifier_generator.rb
+++ lib/generators/rspec/notifier_generator.rb
@@ -1,9 +1,11 @@
 module RSpec
-  class NotifierGenerator < ::Rails::Generators::NamedBase
-    source_root File.expand_path("../templates", __FILE__)
+  module Generators
+    class NotifierGenerator < ::Rails::Generators::NamedBase
+      source_root File.expand_path("../templates", __FILE__)
 
-    def create_spec_file
-      template "notifier_spec.tt", File.join("spec/notifiers", class_path, "#{file_name}_notifier_spec.rb")
+      def create_spec_file
+        template "notifier_spec.tt", File.join("spec/notifiers", class_path, "#{file_name}_notifier_spec.rb")
+      end
     end
   end
 end
